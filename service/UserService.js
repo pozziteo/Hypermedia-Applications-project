aa922d56f7342f39916e94ee19d1b900f9e5d467
Matteo <matteo.pozzi0397@gmail.com>
2019-06-02 00:27:06 +0200

    Removed useless commented block

--- service/UserService.js
+++ service/UserService.js
@@ -87,30 +87,6 @@ exports.userRegisterPOST = function(email, username, password) {
       error.code = 409;
       return error;
     })
-
-
-  /* return sqlDb("customer").select("email")
-    .then(emails => {
-    if (lodash.includes(emails, email)) {
-      console.log("The e-mail you inserted is already in use");
-    } else {
-      return sqlDb("customer").select("username")
-        .then(usernames => {
-        if (lodash.includes(usernames, username)) {
-          console.log("The username you inserted is already in use");
-        } else {
-          return sqlDb("customer")
-            .insert({
-              email: email,
-              password: bcrypt.hashSync(password, 10),
-              username: username
-            })
-        }
-      })
-    }
-  }).catch(error => {
-    return error;
-  }) */
 };
 
 
