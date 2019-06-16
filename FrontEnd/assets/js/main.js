b88986778b404a4b83d16829795a7e59a38d24cb
SambiC <49269867+SambiC@users.noreply.github.com>
2019-06-16 16:51:00 +0200

    new pages and mods

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -1,19 +1,103 @@
 
 
 $(function(){
-    
+
     var $genres = $('#genres');
-    
+
     $.ajax({
         type:'GET',
-        url:'/genre',
+        url:'/v2/books/genres',
         success: function(data){
-        $.each(data, function(i,genre){
-        
-        $genres.append('<div> name:'+genre.name+'</div>');
+            $.each(data, function(i,genre){
+
+                // $genres.append('<img class="singleI" src=""> name:'+genre.name+' - <br> </div>');
+                $genres.append('<div class="col-3 genre"><img class="singleI" src="../assets/img/gen/'+genre.name+'.jpg"><a href=""><span>'+genre.name+'</span> </a></div>');
+            });
+        }
     });
-    }
-    });
-    
-    
+
+
 });
+
+
+
+$(function(){
+
+    var $img = $('#bookImage');
+    var $title = $('#titoloLibro');
+    var $aut = $('#autoreLibro');
+    var $desc = $('#descLibro');
+
+
+
+    $.ajax({
+        type:'GET',
+        url:'/v2/books/9780743477154',
+        success: function(book){
+
+            console.log(book);
+
+            $title.append(book.title);
+            // $aut.append(book.au)
+            $img.attr('src', '../assets/img/'+book.code +'.jpg');
+
+            $desc.html(book.description);
+            $('#prezzoLibro').html('Price: '+book.price.value +' €');
+
+        }
+    });
+
+
+});
+
+
+$(function(){
+
+    $.ajax({
+        type:'GET',
+        url:'/v2/authors/11',
+        success: function(author){
+
+            console.log(author);
+
+            $('#autName').html(author.name);
+
+            $('#authorPic').attr('src', '../assets/img/authors/'+author.author_id +'.jpg');
+
+            $('#bio').html(author.biography);
+
+
+        }
+    });
+
+
+});
+
+
+
+$(function(){
+
+   var $list = $('#books')
+    $.ajax({
+        type:'GET',
+        url:'/v2/books',
+        success: function(data){
+
+            $.each(data, function(i,book){
+
+                console.log(books);
+
+                $list.append('<div class="col-2 singleBook"><a href=""><img src="../assets/img/'+book.code+'.jpg" alt="nnndnd"> <h6>'+ book.title +'</h6><h7>'+ book.author +'</h7></a></div>');
+
+              });                   
+
+            }
+                  
+        });
+
+
+    });
+
+
+
+
