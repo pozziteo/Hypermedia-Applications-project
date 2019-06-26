1b2c0dd2c1a0087ad925eedd7adc22279207b9ac
FrancescoVaghi <francescovaghi97@gmail.com>
2019-06-26 19:38:47 +0200

    Ajax logIn logOut SignUp

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -1,18 +1,18 @@
 $(function(){
 
-    var $genres = $('#genres');
+  var $genres = $('#genres');
 
-    $.ajax({
-        type:'GET',
-        url:'/books/genres',
-        success: function(data){
-            $.each(data, function(i,genre){
+  $.ajax({
+    type:'GET',
+    url:'/books/genres',
+    success: function(data){
+      $.each(data, function(i,genre){
 
-                // $genres.append('<img class="singleI" src=""> name:'+genre.name+' - <br> </div>');
-                $genres.append('<div class="col-3 genre"><img class="singleI" src="../assets/img/gen/'+genre.name+'.jpg"><a href=""><span>'+genre.name+'</span> </a></div>');
-            });
-        }
-    });
+        // $genres.append('<img class="singleI" src=""> name:'+genre.name+' - <br> </div>');
+        $genres.append('<div class="col-3 genre"><img class="singleI" src="../assets/img/gen/'+genre.name+'.jpg"><a href=""><span>'+genre.name+'</span> </a></div>');
+      });
+    }
+  });
 
 
 });
@@ -21,78 +21,29 @@ $(function(){
 
 $(function(){
 
-    var $img = $('#bookImage');
-    var $title = $('#titoloLibro');
-    var $aut = $('#autoreLibro');
-    var $desc = $('#descLibro');
+  var $img = $('#bookImage');
+  var $title = $('#titoloLibro');
+  var $aut = $('#autoreLibro');
+  var $desc = $('#descLibro');
 
 
 
-    $.ajax({
-        type:'GET',
-        url:'/books/9780743477154',
-        success: function(book){
+  $.ajax({
+    type:'GET',
+    url:'/books/9780743477154',
+    success: function(book){
 
-          // console.log(book);
+      // console.log(book);
 
-            $title.append(book.title);
-            // $aut.append(book.au)
-            $img.attr('src', '../assets/img/'+book.code +'.jpg');
+      $title.append(book.title);
+      // $aut.append(book.au)
+      $img.attr('src', '../assets/img/'+book.code +'.jpg');
 
-            $desc.html(book.description);
-            $('#prezzoLibro').html('Price: '+book.price.value +' €');
+      $desc.html(book.description);
+      $('#prezzoLibro').html('Price: '+book.price.value +' €');
 
-        }
-    });
-
-
-});
-
-
-$(function(){
-
-    $.ajax({
-        type:'GET',
-        url:'/authors/11',
-        success: function(author){
-
-            console.log(author);
-
-            $('#autName').html(author.name);
-
-            $('#authorPic').attr('src', '../assets/img/authors/'+author.author_id +'.jpg');
-
-            $('#bio').html(author.biography);
-
-
-        }
-    });
-
-
-});
-
-
-
-
-$(function(){
-
-    var $list = $('#books')
-    $.ajax({
-        type:'GET',
-        url:'/books',
-        success: function(data){
-
-            $.each(data, function(i,book){
-
-                //console.log(books);
-
-                $list.append('<div class="col-2 singleBook"><a href=""><img src="../assets/img/'+book.code+'.jpg" alt="nnndnd"> <h6>'+ book.title +'</h6><h7>'+ book.author +'</h7></a></div>');
-
-            });
-
-        }
-
-    });
+    }
+  });
 
 
 });
@@ -100,47 +51,96 @@ $(function(){
 
 $(function(){
 
-    var $list = $('#gFilters');
-    $.ajax({
-        type:'GET',
-        url:'/books/genres',
-        success: function(data){
+  $.ajax({
+    type:'GET',
+    url:'/authors/11',
+    success: function(author){
 
-            $.each(data, function(i,genre){
+      console.log(author);
 
-               // console.log(genre);
+      $('#autName').html(author.name);
+
+      $('#authorPic').attr('src', '../assets/img/authors/'+author.author_id +'.jpg');
+
+      $('#bio').html(author.biography);
 
 
-                $list.append("<button class="+"btn"+" onclick="+"filterSelection('genre','"+genre.name+"')"+">"+genre.name+"</button>");
+    }
+  });
 
-            });
 
-        }
+});
 
-    });
+
+
+
+$(function(){
+
+  var $list = $('#books')
+  $.ajax({
+    type:'GET',
+    url:'/books',
+    success: function(data){
+
+      $.each(data, function(i,book){
+
+        //console.log(books);
+
+        $list.append('<div class="col-2 singleBook"><a href=""><img src="../assets/img/'+book.code+'.jpg" alt="nnndnd"> <h6>'+ book.title +'</h6><h7>'+ book.author +'</h7></a></div>');
+
+      });
+
+    }
+
+  });
+
+
+});
+
+
+$(function(){
+
+  var $list = $('#gFilters');
+  $.ajax({
+    type:'GET',
+    url:'/books/genres',
+    success: function(data){
+
+      $.each(data, function(i,genre){
+
+        // console.log(genre);
+
+
+        $list.append("<button class="+"btn"+" onclick="+"filterSelection('genre','"+genre.name+"')"+">"+genre.name+"</button>");
+
+      });
+
+    }
+
+  });
 
 
 });
 
 $(function(){
 
-    var $list = $('#tFilters');
-    $.ajax({
-        type:'GET',
-        url:'/books/themes',
-        success: function(data){
+  var $list = $('#tFilters');
+  $.ajax({
+    type:'GET',
+    url:'/books/themes',
+    success: function(data){
 
-            $.each(data, function(i,theme){
+      $.each(data, function(i,theme){
 
-               // console.log(urlString(theme.name));
+        // console.log(urlString(theme.name));
 
-                $list.append("<button class="+"btn"+" onclick="+"filterSelection('theme','"+urlString(theme.name)+"')"+">"+theme.name+"</button>");
+        $list.append("<button class="+"btn"+" onclick="+"filterSelection('theme','"+urlString(theme.name)+"')"+">"+theme.name+"</button>");
 
-            });
+      });
 
-        }
+    }
 
-    });
+  });
 
 
 });
@@ -148,31 +148,31 @@ $(function(){
 
 function filterSelection(type, nome){
 
-     document.getElementById("gFilters").style.backgroundColor = "red";
+  document.getElementById("gFilters").style.backgroundColor = "red";
 
 
-    console.log(type);
-    var $list = $('#books');
-   // var $genre="Fatasy"
-    $.ajax({
-        type:'GET',
-        url:'/books?'+type+'='+nome+'',
-        success: function(data){
+  console.log(type);
+  var $list = $('#books');
+  // var $genre="Fatasy"
+  $.ajax({
+    type:'GET',
+    url:'/books?'+type+'='+nome+'',
+    success: function(data){
 
-            $list.html("");
+      $list.html("");
 
-            $.each(data, function(i,book){
+      $.each(data, function(i,book){
 
 
-                //console.log(book);
+        //console.log(book);
 
-                $list.append('<div class="col-2 singleBook"><a href=""><img src="../assets/img/'+book.code+'.jpg" alt="nnndnd"> <h6>'+ book.title +'</h6><h7>'+ book.author +'</h7></a></div>');
+        $list.append('<div class="col-2 singleBook"><a href=""><img src="../assets/img/'+book.code+'.jpg" alt="nnndnd"> <h6>'+ book.title +'</h6><h7>'+ book.author +'</h7></a></div>');
 
-            });
+      });
 
-        }
+    }
 
-    });
+  });
 
 
 };
@@ -184,16 +184,16 @@ function filterSelection(type, nome){
 
 function urlString(text){
 
-    var array = text.split(" ");
-    var newString=array[0];
+  var array = text.split(" ");
+  var newString=array[0];
 
-    for(i=1; i< array.length; i++){
+  for(i=1; i< array.length; i++){
 
-       newString+="%20"+array[i];
-    }
+    newString+="%20"+array[i];
+  }
 
 
-    return newString;
+  return newString;
 };
 
 
@@ -205,34 +205,34 @@ Function for events page
 
 $(function(){
 
-    var $list = $('#events');
-    $.ajax({
-        type:'GET',
-        url:'/events',
-        success: function(data){
+  var $list = $('#events');
+  $.ajax({
+    type:'GET',
+    url:'/events',
+    success: function(data){
 
-            $.each(data, function(i,event){
+      $.each(data, function(i,event){
 
-                console.log(event.id);
+        console.log(event.id);
 
-                $list.append("<div class='event'><div class='ribbon'>"+ dateSplit(event.date)+"</div> <div><a href=''><img src='../assets/img/events/"+event.event_id+".jpg' alt='Event Image'></a><div class='overlayInfo'><h5> <a href=''>"+ event.title +"</a></h5><h6> <a href=''>"+ event.book.title +"</a>, by <a href=''>"+ event.book.authors[0].name +"</a></h6><p>"+ event.place+"</p></div></div></div>");
+        $list.append("<div class='event'><div class='ribbon'>"+ dateSplit(event.date)+"</div> <div><a href=''><img src='../assets/img/events/"+event.event_id+".jpg' alt='Event Image'></a><div class='overlayInfo'><h5> <a href=''>"+ event.title +"</a></h5><h6> <a href=''>"+ event.book.title +"</a>, by <a href=''>"+ event.book.authors[0].name +"</a></h6><p>"+ event.place+"</p></div></div></div>");
 
-            });
+      });
 
-        }
+    }
 
-    });
+  });
 
 
 });
 
 function dateSplit(text){
 
-    var array = text.split("T");
-    var date=array[0].split("-");
-    var newDate=date[1]+"-"+date[2]+"-"+date[0];
+  var array = text.split("T");
+  var date=array[0].split("-");
+  var newDate=date[1]+"-"+date[2]+"-"+date[0];
 
-    return newDate;
+  return newDate;
 
 };
 
@@ -242,23 +242,23 @@ Function for BestSeller in Home Page
 ----------------*/
 
 $(function(){
-    var $list = $('#bestSellerContainer');
-    $.ajax({
-        type:'GET',
-        url:'/books/best-sellers',
-        success: function(data){
+  var $list = $('#bestSellerContainer');
+  $.ajax({
+    type:'GET',
+    url:'/books/best-sellers',
+    success: function(data){
 
-            $.each(data, function(i, bestSeller){
+      $.each(data, function(i, bestSeller){
 
-            if(i == 0){
-                $list.append('<div id="firstBestSeller" class="bestSeller row"><div class="col-4"><img src="assets/img/'+ bestSeller.code +'.jpg" alt="'+ bestSeller.title +'"></div><div class="col-7"><p id="firstBestSellerNumber">#1</p><span><p>'+ bestSeller.title +'</p><p>'+ bestSeller.author +'</p><p>'+ bestSeller.value +'</p><span></div></div>');
-            }
-            else{
-                $list.append('<div class="bestSeller"><p class="bestSellerNumber">#2</p><span><p>'+ bestSeller.title +'</p><p>'+ bestSeller.author +'</p><p>'+ bestSeller.value +'</p></span></div>');
-                }
-            });
+        if(i == 0){
+          $list.append('<div id="firstBestSeller" class="bestSeller row"><div class="col-4"><img src="assets/img/'+ bestSeller.code +'.jpg" alt="'+ bestSeller.title +'"></div><div class="col-7"><p id="firstBestSellerNumber">#1</p><span><p>'+ bestSeller.title +'</p><p>'+ bestSeller.author +'</p><p>'+ bestSeller.value +'</p><span></div></div>');
         }
-    });
+        else{
+          $list.append('<div class="bestSeller"><p class="bestSellerNumber">#2</p><span><p>'+ bestSeller.title +'</p><p>'+ bestSeller.author +'</p><p>'+ bestSeller.value +'</p></span></div>');
+        }
+      });
+    }
+  });
 });
 
 
@@ -267,22 +267,22 @@ Function for OurFavourite in Home Page
 ----------------*/
 
 $(function(){
-    var $left = $('#leftOurFavourite');
-    var $right = $('#rightOurFavourite');
-    $.ajax({
-        type:'GET',
-        url:'/books',
-        success: function(data){
-            $.each(data, function(i, ourFavourite){
-                if(i%2 == 0){
-                    $left.append('<div class="ourFavourite row"><div class="col"><img src="assets/img/'+ ourFavourite.code +'.jpg" alt="'+ ourFavourite.title +'"></div><div class="col"><span><p>'+ ourFavourite.title +'</p><p>'+ ourFavourite.author +'</p><p>'+ ourFavourite.value +'</p></span></div></div>');
-                }
-                else{
-                    $right.append('<div class="ourFavourite row"><div class="col"><img src="assets/img/'+ ourFavourite.code +'.jpg" alt="'+ ourFavourite.title +'"></div><div class="col"><span><p>'+ ourFavourite.title +'</p><p>'+ ourFavourite.author +'</p><p>'+ ourFavourite.value  +'</p></span></div></div>');
-                }
-            });
+  var $left = $('#leftOurFavourite');
+  var $right = $('#rightOurFavourite');
+  $.ajax({
+    type:'GET',
+    url:'/books/our-favourites',
+    success: function(data){
+      $.each(data, function(i, ourFavourite){
+        if(i%2 == 0){
+          $left.append('<div class="ourFavourite row"><div class="col"><img src="assets/img/'+ ourFavourite.code +'.jpg" alt="'+ ourFavourite.title +'"></div><div class="col"><span><p>'+ ourFavourite.title +'</p><p>'+ ourFavourite.author +'</p><p>'+ ourFavourite.value +'</p></span></div></div>');
         }
-    });
+        else{
+          $right.append('<div class="ourFavourite row"><div class="col"><img src="assets/img/'+ ourFavourite.code +'.jpg" alt="'+ ourFavourite.title +'"></div><div class="col"><span><p>'+ ourFavourite.title +'</p><p>'+ ourFavourite.author +'</p><p>'+ ourFavourite.value  +'</p></span></div></div>');
+        }
+      });
+    }
+  });
 });
 
 /*----------------
@@ -290,19 +290,20 @@ Function for SignUp in SignUp page
 ----------------*/
 
 $(document).ready(function(){
-  var $mail = $('#signUpEmail');
-  var $username = $('#signUpUsername');
-  var $psw = $('#signUpPassword');
   $("#signUpButton").click(function () {
+    var $mail = $('#signUpEmail');
+    var $username = $('#signUpUsername');
+    var $psw = $('#signUpPassword');
     $.ajax({
       type: "POST",
       url:'/user/register',
       data:jQuery.param({email:$mail.val(),username:$username.val(),password:$psw.val()}),
-      success: function () {
-        alert("il Sign Up ha funzionato");
+      success: function (response) {
+        alert("You have successfully registered");
+        window.open("../index.html", "index.html");
       },
       error: function (response) {
-        alert("non funza");
+        alert("Sign Up failed");
       }
     });
   });
@@ -314,45 +315,62 @@ Function for LogIn in LogIn page
 ----------------*/
 
 $(document).ready(function(){
-  var $mail = $('#logInEmail');
+  var $username = $('#logInUsername');
   var $psw = $('#logInPassword');
   $("#logInButton").click(function () {
     $.ajax({
       type: "POST",
       url:'/user/login',
-      data:jQuery.param({username:$mail.val(),password:$psw.val()}),
+      data:jQuery.param({username:$username.val(),password:$psw.val()}),
       success: function () {
-        alert("Ho funzionato");
+        alert("You have successfully logged in");
+        window.open("../index.html", "index.html");
       },
       error: function (response) {
-        alert("non funza");
+        alert("Log In failed");
       }
     });
   });
 });
 
-
 /*----------------
-Function for User Cart Item in Cart page
+Function for LogOut in Home page
 ----------------*/
+$(document).ready(function(){
+  $("#logOutButton").click(function() {
+    $.ajax({
+      type: "POST",
+      url : "/user/logout",
+      success: function() {
+        alert("You have successfully logged out");
+      },
+      error: function() {
+        alert("Log Out failed");
+      }
+    });
+  })});
 
-$(function(){
+  /*----------------
+  Function for User Cart Item in Cart page
+
+  $(function(){
   var $cart = $('#cartList');
   var $buy = $('#buyCart');
-    $.ajax({
-        type:'GET',
-        url:'/cart',
-        success: function(data){
-            $.each(data, function(i, cartItem){
-                if(i == 0){
-                  $cart.append('<div class="cartItem"><img src="../assets/img' + cartItem.code + '.jpg" alt="' + cartItem.title + '"><span class="cartItemInfo"><a class="cartItemTitle" href="">' + cartItem.title + '</a> by <a class="cartItemAuthor" href="">' + cartItem.author + '</a><p class="cartItemPrice">EUR ' + cartItem.price + '</p><a class="cartItemRemove" href="#">remove</a></span></div>');
-                }
-                else{
-                  $cart.append('<div class="cartItem"><hr><img src="../assets/img' + cartItem.code + '.jpg" alt="' + cartItem.title + '"><span class="cartItemInfo"><a class="cartItemTitle" href="">' + cartItem.title + '</a> by <a class="cartItemAuthor" href="">' + cartItem.author + '</a><p class="cartItemPrice">EUR ' + cartItem.price + '</p><a class="cartItemRemove" href="#">remove</a></span></div>');
+  $.ajax({
+  type:'GET',
+  url:'/cart',
+  success: function(data){
+  $.each(data, function(i, cartItem){
+  if(i == 0){
+  $cart.append('<div class="cartItem"><img src="../assets/img' + cartItem.code + '.jpg" alt="' + cartItem.title + '"><span class="cartItemInfo"><a class="cartItemTitle" href="">' + cartItem.title + '</a> by <a class="cartItemAuthor" href="">' + cartItem.author + '</a><p class="cartItemPrice">EUR ' + cartItem.price + '</p><a class="cartItemRemove" href="#">remove</a></span></div>');
+}
+else{
+$cart.append('<div class="cartItem"><hr><img src="../assets/img' + cartItem.code + '.jpg" alt="' + cartItem.title + '"><span class="cartItemInfo"><a class="cartItemTitle" href="">' + cartItem.title + '</a> by <a class="cartItemAuthor" href="">' + cartItem.author + '</a><p class="cartItemPrice">EUR ' + cartItem.price + '</p><a class="cartItemRemove" href="#">remove</a></span></div>');
 
-                }
-            });
-            $buy.append('<p>Totale Provvisorio (' + data.length + 'articolo)</p><p>EUR ' + data + '</p><button type="button" type="submit">Complete your order</button>');
-        }
-    });
+}
 });
+$buy.append('<p>Totale Provvisorio (' + data.length + 'articolo)</p><p>EUR ' + data + '</p><button type="button" type="submit">Complete your order</button>');
+}
+});
+});
+----------------*/
